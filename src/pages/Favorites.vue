71be3cb1f24fcc2f16b7240810ca8a493db92601
VitaliyJC <vitaliy.jc@yandex.ru>
2024-01-14 12:46:36 +0500

    vue market v1.0

---
+++ src/pages/Favorites.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+import CardList from '../components/CardList.vue'
+
+const favorites = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get(
+      'https://6eb77a5d8efe1fc8.mokky.dev/favorites?_relations=items'
+    )
+    favorites.value = data.map((obj) => obj.item)
+  } catch (error) {
+    console.log(error)
+  }
+})
+</script>
+
+<template>
+  <h2 class="text-3xl font-bold mb-8">Мои закладки</h2>
+  <CardList :items="favorites" is-favorites />
+</template>
